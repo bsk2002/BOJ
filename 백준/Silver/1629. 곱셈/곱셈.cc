bd6a2668cbd3153ae368ef964cf0bb70116857e5
sangkyoung <69530570+bsk2002@users.noreply.github.com>
2024-10-07 21:50:02 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+ll pow(ll a, ll b, ll m) {
+  if (b == 1) return a % m;
+  ll val = pow(a, b / 2, m);
+  val = val * val % m;
+  if (b % 2 == 0) return val;
+  return val * a % m;
+}
+
+int main() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  cout << pow(a, b, c);
+}
