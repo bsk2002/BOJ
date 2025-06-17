590bd1f6d1958ceb484ce29cf4d21e720a476fd6
sangkyoung <69530570+bsk2002@users.noreply.github.com>
2025-06-17 14:51:00 +0900

    [Silver IV] Title: 덱 2, Time: 156 ms, Memory: 6308 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    deque<int> dq;
+
+    int x;
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        switch (k)
+        {
+        case 1:
+            cin >> x;
+            dq.push_front(x);
+            break;
+        case 2:
+            cin >> x;
+            dq.push_back(x);
+            break;
+        case 3:
+            if (!dq.empty()) {
+                cout << dq.front() << "\n";
+                dq.pop_front();
+            }
+            else cout << "-1\n";
+            break;
+        case 4:
+            if (!dq.empty()) {
+                cout << dq.back() << "\n";
+                dq.pop_back();
+            }
+            else cout << "-1\n";
+            break;
+        case 5:
+            cout << dq.size() << "\n";
+            break;
+        case 6:
+            if (dq.empty()) cout << "1\n";
+            else cout << "0\n";
+            break;
+        case 7:
+            if (!dq.empty()) {
+                cout << dq.front() << "\n";
+            }
+            else cout << "-1\n";
+            break;
+        case 8:
+            if (!dq.empty()) {
+                cout << dq.back() << "\n";
+            }
+            else cout << "-1\n";
+            break;
+        default:
+            break;
+        }
+    }
+}
