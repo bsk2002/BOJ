a12f1149a996097df5d8a2ff7846f100437b9b56
sangkyoung <69530570+bsk2002@users.noreply.github.com>
2025-06-29 10:04:14 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string str;
+
+    cin.ignore();
+
+    for (int i = 1; i <= n; i++) {
+        getline(cin, str);
+        cin.clear();
+        cout << i << ". " << str << "\n";
+    }
+}
